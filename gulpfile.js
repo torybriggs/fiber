c5fb3cfed7064d5c1228d7bf5db1f33af72e1eaa
Brian Hough <brian.r.hough@gmail.com>
2015-03-07 10:33:08 -0800

    Browser-sync server task with regex for cachebuting

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
 var gulp = require('gulp');
 var config = require('./config.json');
 var plugins = require('gulp-load-plugins')({
-  pattern: ['gulp-*', 'gulp.*', 'del'] 
+  pattern: ['gulp-*', 'gulp.*', 'del', 'browser-sync'] 
 });
 
-//File Cleanup
+// File Cleanup
 require('./tasks/clean')(gulp, plugins);
 
- //SASS Compilation
+ // SASS Compilation
 require('./tasks/csscompile')(gulp, plugins, config);
 
- //JS Minification
+ // JS Minification
 require('./tasks/uglify')(gulp, plugins, config);
 
-//Image Minifications
+// Image Minifications
 require('./tasks/imagemin')(gulp, plugins, config);
 
-//Cachebust CSS
+// Cachebust CSS
 require('./tasks/cachebust')(gulp, plugins);
 
-//Watch For Changes
+// Basic Server
+require('./tasks/browserSync')(gulp, plugins);
+
+// Watch For Changes
 require('./tasks/watch')(gulp, plugins, config);
 
-//Default Task Triggers Watch
+// Default Task Triggers Watch
 gulp.task('default', function() {
-    gulp.start('watch');
+    gulp.start('browser-sync', 'watch');
 });
---
+++ tasks/browserSync.js
@@ -0,0 +1,15 @@
+module.exports = function(gulp, plugins) {
+  
+    gulp.task('browser-sync', function() {
+        plugins.browserSync({
+            server: {
+                baseDir: "./",
+                middleware: function (req, res, next) {
+                    req.url = req.url.replace(/\/([^\/]+)\.[0-9a-f]+\.(css|js|jpg|png|gif|svg)$/, "/$1.$2");
+                    next();
+                }
+            }
+        });
+    });
+
+};
