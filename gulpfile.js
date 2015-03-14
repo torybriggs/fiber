673a86edd4ed4a04a0151ec477862e23c6d0c951
Brian Hough <brian.r.hough@gmail.com>
2015-03-14 16:59:05 -0700

    add .editorconfig, move tasks and configuration into src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
 var gulp = require('gulp');
-var config = require('./config.json');
+var config = require('./src/tasks/config.json');
 
 var plugins = require('gulp-load-plugins')({
   pattern: ['*'] 
 });
 
 // BrowserSync Reload
-require('./tasks/reload')(gulp, plugins);
+require('./src/tasks/reload')(gulp, plugins);
 
 // File Cleanup CSS
-require('./tasks/cleanCss')(gulp, plugins, config);
+require('./src/tasks/cleanCss')(gulp, plugins, config);
 
 // File Cleanup JS
-require('./tasks/cleanJs')(gulp, plugins, config);
+require('./src/tasks/cleanJs')(gulp, plugins, config);
 
  // SASS Compilation
-require('./tasks/csscompile')(gulp, plugins, config);
+require('./src/tasks/csscompile')(gulp, plugins, config);
 
  // JS Bundling
-require('./tasks/browserify')(gulp, plugins, config);
+require('./src/tasks/browserify')(gulp, plugins, config);
 
 // Image Minifications
-require('./tasks/imagemin')(gulp, plugins, config);
+require('./src/tasks/imagemin')(gulp, plugins, config);
 
 // Cachebust CSS
-require('./tasks/cachebust')(gulp, plugins);
+require('./src/tasks/cachebust')(gulp, plugins);
 
 // Sourcemap Reference
-require('./tasks/mapReference')(gulp, plugins);
+require('./src/tasks/mapReference')(gulp, plugins);
 
 // Basic Server
-require('./tasks/browserSync')(gulp, plugins);
+require('./src/tasks/browserSync')(gulp, plugins);
 
 // CSS Build
-require('./tasks/cssBuild')(gulp, plugins);
+require('./src/tasks/cssBuild')(gulp, plugins);
 
 // CSS Build
-require('./tasks/jsBuild')(gulp, plugins);
+require('./src/tasks/jsBuild')(gulp, plugins);
 
 // Watch For Changes
-require('./tasks/watch')(gulp, plugins, config);
+require('./src/tasks/watch')(gulp, plugins, config);
 
 // Default Task Triggers Watch
 gulp.task('default', function() {
